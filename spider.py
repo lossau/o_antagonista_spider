47a314ac4ed73968164342566da0e234856db24e
Saulo Mendes <sauloml@gmail.com>
2017-07-19 15:27:17 -0300

    use try/except for possible data missing

--- spider.py
+++ spider.py
@@ -5,25 +5,33 @@ import scrapy
 
 class OAntagonistaSpider(scrapy.Spider):
     name = 'o_antagonista'
-    quotes_base_url = 'http://www.oantagonista.com/pagina/%s'
+    base_url = 'http://www.oantagonista.com/pagina/%s'
     current_page = 1
-    last_page = 10
-    start_urls = [quotes_base_url % current_page]
+    last_page = 5
+    start_urls = [base_url % current_page]
     download_delay = 1.5
-    blog_posts = []
+    posts = []
 
     def parse(self, response):
 
         for post in response.css('article.post'):
 
-            post_date = post.css('time ::text').extract_first()
-            post_date = datetime.datetime.strptime(post_date, '%d.%m.%y').strftime('%d/%m/%y')
+            try:
+                post_date = datetime.datetime.strptime(
+                    post.css('time ::text').extract_first(), '%d.%m.%y').strftime('%d/%m/%y')
+            except:
+                post_date = ''
 
-            self.blog_posts.append({
+            try:
+                post_time = post.css('span ::text')[4].extract().strip()
+            except:
+                post_time = ''
+
+            self.posts.append({
                 'id': post.css('article.post ::attr(id)').extract_first(),
                 'title': post.css('a ::text').extract_first(),
                 'summary': post.css('p ::text').extract_first(),
-                'time': post.css('span ::text')[4].extract().strip(),
+                'time': post_time,
                 'date': post_date,
                 'page': self.current_page})
 
@@ -32,8 +40,8 @@ class OAntagonistaSpider(scrapy.Spider):
                 existing_posts = json.loads(f.read())
             except:
                 existing_posts = []
-            f.write(json.dumps(existing_posts + self.blog_posts))
+            f.write(json.dumps(existing_posts + self.posts))
 
         if self.current_page < self.last_page:
             self.current_page = self.current_page + 1
-            yield scrapy.Request(self.quotes_base_url % self.current_page)
+            yield scrapy.Request(self.base_url % self.current_page)
